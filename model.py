7cb36fb1ee13a27154512066b57259cf70a49a4f
Ilya Kostrikov <ikostrikov@gmail.com>
2017-02-13 15:48:18 -0500

    Fix a typo in the definition of the network

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ class ActorCritic(torch.nn.Module):
 
     def __init__(self, num_inputs, action_space):
         super(ActorCritic, self).__init__()
-        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2)
+        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
         self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
